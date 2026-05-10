3808d4181045f921a164b34ec824f773c6e45c7a
julber392 <supermatrix39@gmail.com>
2026-05-11 00:33:36 +0300

    gameover-ui-smooth

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,30 +1,62 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private GameObject _gameOverPanel;
+    [SerializeField] private CanvasGroup _canvasGroup;
+
     [SerializeField] private string _menuSceneName = "Menu";
     [SerializeField] private PlayerHp _playerHp;
     [SerializeField] private PauseManager _pauseManager;
 
+    [SerializeField] private float fadeDuration = 1f;
+
     private void Awake()
     {
         _gameOverPanel.SetActive(false);
+
+        _canvasGroup.alpha = 0f;
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
     }
 
     public void ShowGameOver()
     {
         _gameOverPanel.SetActive(true);
+
+        StartCoroutine(FadeRoutine());
+    }
+
+    private IEnumerator FadeRoutine()
+    {
+        float timer = 0f;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.unscaledDeltaTime;
+
+            _canvasGroup.alpha = timer / fadeDuration;
+
+            yield return null;
+        }
+
+        _canvasGroup.alpha = 1f;
+
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+
         _pauseManager.Pause("gameover");
     }
 
     public void BackToMenu()
     {
         _pauseManager.Resume("gameover");
+
         SceneManager.LoadScene(_menuSceneName);
     }
-    
+
     private void OnEnable()
     {
         _playerHp.OnDiedPlayer += ShowGameOver;
